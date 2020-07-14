6e316ca4e85530287217c558f1927026cf1710ac
Omar Khan <ok2282@columbia.edu>
2020-07-14 14:44:51 -0500

    Calendar

--- src/App.js
+++ src/App.js
@@ -6,18 +6,35 @@ import Handsontable from 'handsontable';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.handsontableData = Handsontable.helper.createSpreadsheetData(13, 2); 
+    this.data = [
+      ["", "Scheduling One", "Scheduling Two"],
+      ["8 AM", "free", "free"],
+      ["9 AM", "free", "free"],
+      ["10 AM", "free", "free"],
+      ["11 AM", "free", "free"],
+      ["12 PM", "free", "free"],
+      ["1 PM", "free", "free"],
+      ["2 PM", "free", "free"],
+      ["3 PM", "free", "free"],
+      ["4 PM", "free", "free"],
+      ["5 PM", "free", "free"],
+      ["6 PM", "free", "free"],
+      ["7 PM", "free", "free"],
+      ["8 PM", "free", "free"]
+    ];
   }
 
   render() {
     return (
       <div>
         <HotTable 
+          licenseKey="non-commercial-and-evaluation"
           id="hot"
-          data={this.handsontableData} 
-          colHeaders={true} 
-          rowHeaders={true}
-          />
+          data={this.data} 
+          colHeaders={false} 
+          rowHeaders={false}
+          width="600"
+        />
       </div>
     );
   }
